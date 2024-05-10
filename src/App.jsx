79f22410d237b14f7b7cd47cac57ef6e9027555a
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-10 15:38:47 +0200

    ✨ App > install routin system

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+  return <Routes>
+    <Route path="/" element={<Home />}/>
+  </Routes>
 }
 
 export default App
