f7c093c07f000bbd5999f60b05cd9be52901977d
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-11 10:45:29 +0200

    ✨ App > Error > Create Error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Dashboard from "./pages/dashboard/Dashboard";
+import Error from "./pages/error/Error";
 
 function App() {
   return <Routes>
     <Route path="/" element={<Home />}/>
     <Route path="/dashboard/:id" element={<Dashboard />}/>
+    <Route path="*" element={<Error />}/>
   </Routes>
 }
 
