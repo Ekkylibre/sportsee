a1aa58abbdc5bcf4c06d6ebd40c329aa66fd4d3f
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-22 18:56:58 +0200

    🚧 Components > ValueItem > Rework code

--- src/components/valueItem/ValueItem.jsx
+++ src/components/valueItem/ValueItem.jsx
@@ -1,11 +1,13 @@
 import './valueItem.css';
 
 function ValueItem({ number, title, nutrientIcon }) {
+  const displayNumber = number ? number : 'N/A';
+
   return (
     <div className="value">
       <div>{nutrientIcon}</div>
       <div>
-        <div className="number">{number}</div>
+        <div className="number">{displayNumber}</div>
         <div className="title">{title}</div>
       </div>
     </div>
