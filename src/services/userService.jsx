20662528c0567e05a78b321ea6529bfaee12ada2
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-22 19:07:22 +0200

    🚧  Services > userService > Rework code

--- src/services/userService.jsx
+++ src/services/userService.jsx
@@ -2,14 +2,15 @@ import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE 
 import UserDataModel from '../models/UserDataModel';
 
 const fetchUserData = (userId) => {
-    const userData = USER_MAIN_DATA.find(user => user.id === parseInt(userId, 10));
+    const userIdInt = parseInt(userId);
+    const userData = USER_MAIN_DATA.find(user => user.id === userIdInt);
     if (!userData) {
         throw new Error('User not found');
     }
 
-    const userActivity = USER_ACTIVITY.find(activity => activity.userId === parseInt(userId, 10));
-    const userAverageSessions = USER_AVERAGE_SESSIONS.find(session => session.userId === parseInt(userId, 10));
-    const userPerformance = USER_PERFORMANCE.find(performance => performance.userId === parseInt(userId, 10));
+    const userActivity = USER_ACTIVITY.find(activity => activity.userId === userIdInt);
+    const userAverageSessions = USER_AVERAGE_SESSIONS.find(session => session.userId === userIdInt);
+    const userPerformance = USER_PERFORMANCE.find(performance => performance.userId === userIdInt);
 
     if (!userActivity || !userAverageSessions || !userPerformance) {
         throw new Error('Related data not found');
