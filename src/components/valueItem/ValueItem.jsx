62b5ad68813c8885d41ba6d86abcb9bfb60a2a65
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-23 19:05:22 +0200

    🚧 Components > ValueIte > Rework code error message

--- src/components/valueItem/ValueItem.jsx
+++ src/components/valueItem/ValueItem.jsx
@@ -1,13 +1,14 @@
 import './valueItem.css';
 
-function ValueItem({ number, title, nutrientIcon }) {
-  const displayNumber = number ? number : 'N/A';
+function ValueItem({ value, title, nutrientIcon }) {
+  // Vérifie si la valeur est un nombre ou une chaîne de caractères
+  const displayValue = value !== undefined && value !== null ? value : 'N/A';
 
   return (
     <div className="value">
       <div>{nutrientIcon}</div>
       <div>
-        <div className="number">{displayNumber}</div>
+        <div className="value-display">{displayValue}</div>
         <div className="title">{title}</div>
       </div>
     </div>
