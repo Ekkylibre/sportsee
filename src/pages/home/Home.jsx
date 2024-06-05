1be486ff9384a3490cd0ab0c13ba024a2f41a875
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-06-05 18:01:33 +0200

    ♻️ Pages > Home > Refactor code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,8 @@ import NavBar from "../../components/navBar/NavBar";
 import SideBar from "../../components/verticalBar/VerticalBar";
 import "./home.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import Toggle from "../../components/toggle/Toggle";
 
 function Home() {
-    const [useApiData, setUseApiData] = useState(true); // État pour basculer entre API et données statiques
-
-    const handleToggle = () => {
-        setUseApiData(!useApiData);
-    };
 
     return (
         <>
@@ -22,12 +15,9 @@ function Home() {
                         <h1>Bienvenue sur <span className="red">SportSee</span></h1>
                         <p>Choisissez un utilisateur ci-dessous pour accéder au tableau de bord 😄</p>
                     </div>
-
-                    <Toggle checked={useApiData} onChange={handleToggle} />
-
                     <div className="user-btn">
-                        <Link to={`/dashboard/user/12?useApiData=${useApiData}`}><button>User 12</button></Link>
-                        <Link to={`/dashboard/user/18?useApiData=${useApiData}`}><button>User 18</button></Link>
+                        <Link to="/dashboard/user/12"><button>User 12</button></Link>
+                        <Link to="/dashboard/user/18"><button>User 18</button></Link>
                     </div>
                 </div>
             </main>
